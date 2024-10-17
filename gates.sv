49df9a3951f16f6ea17dda5b8c350933f88b3344
citrus-arch <tattvamanjari0145@gmail.com>
2024-10-17 21:02:58 +0530

    Add files via upload

---
+++ gates.sv
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.10.2024 20:35:53
+// Design Name: 
+// Module Name: gates
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module gates(
+    input logic  a,
+    output y
+    );
+    assign y=~a;
+endmodule
